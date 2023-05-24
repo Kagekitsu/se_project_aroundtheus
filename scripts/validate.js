63ae474d45318bd26824669c630260ab0598addf
kage <baj147@yahoo.com>
2023-05-24 14:19:16 -0400

    added enableValidation(config) to validation.js

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,12 @@
-enableValidation ({
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible"
-});
+
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".button__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible"
+};
+
+enableValidation(config);
