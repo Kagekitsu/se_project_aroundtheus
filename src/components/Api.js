345188b6f4f2c32b0350ce9a184d7dd8f9459ce0
Braden <baj147@yahoo.com>
2023-06-24 14:01:27 -0400

    updated Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -4,7 +4,18 @@ class Api {
     }
 
     getInitialCards() {
+        return fetch('https://around.nomoreparties.co/v1/cohort-3-en', {
+            headers: {
+                authorization: 'd8b9199f-b9d7-4b7f-ad09-c5597d55941e'
+            }
+        })
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
 
+            return Promise.reject(`Error: ${res.status}`);
+        })
     }
 }
 
