01f4de1b89cbecdb07a76abb440b47808ff80fc4
Braden <baj147@yahoo.com>
2023-06-28 13:03:14 -0400

    updated PopupWithForm.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,6 +5,17 @@ export default class PopupWithForm extends Popup {
     super({ popupSelector });
     this._handleFormSubmit = handleFormSubmit;
     this._popupForm = this._popupElement.querySelector('.modal__form');
+    this._modalInputs = this._popupElement.querySelectorAll('.modal_input');
+    this._submitBtn = this._popupElement.querySelector('.modal__button');
+    this._submitBtnTxt = this._submitBtn.textContent;
+  }
+
+  renderLoading(isLoading, loadingText = "Saving...") {
+    if (isLoading) {
+      this._submitBtn.textContent = loadingText;
+    } else {
+      this._submitBtn.textContent = this._submitBtnTxt
+    }
   }
 
   _getInputValues() {
