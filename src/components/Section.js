00dda476a94e7b22f888f13f72e7a30642dca0bb
Braden <baj147@yahoo.com>
2023-06-28 22:08:42 -0400

    updated index.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,22 @@
 export default class Section {
-    constructor({ renderer }, containerSelector) {
-      this._renderer = renderer;
-      this._container = document.querySelector(containerSelector);
-    }
-  
-    renderItems() {
-      this._renderItems.forEach((item) => {
-        this._renderer(item);
-      });
-    }
-  
-    addItem(element) {
-      this._container.append(element);
-    }
-
-    prependItem(element) {
-      this._container.prepend(element);
-    }
+  constructor({ renderer }, containerSelector) {
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
   }
+
+  renderItems(items) {
+    items.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    this._container.append(element);
+  }
+
+  prependItem(element) {
+    this._container.prepend(element);
+  }
+}
+
   
